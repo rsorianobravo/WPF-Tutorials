18891c5aff242758c91c381cd87368be70b87705
raulsb <raul.soriano@nunegal.com>
2023-11-04 10:37:57 +0100

    [ADD] Add commands

--- WPF Switch MVVM/ViewModels/MainViewModel.cs
+++ WPF Switch MVVM/ViewModels/MainViewModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using WPF_Switch_MVVM.Commands;
 
 namespace WPF_Switch_MVVM.ViewModels
 {
@@ -10,10 +12,15 @@ namespace WPF_Switch_MVVM.ViewModels
     {
         public BaseViewModel SelectedViewModel { get; set; }
 
+        public ICommand UpdateViewCommand { get; set; }
+
         public MainViewModel()
         {
             SelectedViewModel = new HomeViewModel();
+            UpdateViewCommand = new UpdateViewCommand(this);
+
         }
+
 
     }
 }
